0c6d8ef795a31ced2065e3d57432e760f5cde02d
Ryley Herrington <ryleyherrington@gmail.com>
2013-10-09 21:55:41 -0700

    slightly more code added.

--- perc.c
+++ perc.c
@@ -1,57 +1,76 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 
-struct Data {
+class Data {
+  public:
+  int y;
+  float x1, x2, x3;
+
+
    int valid;
    float x,y,z;
 };
 
-int dotprod(std::vector<float> x, std::vector<float> w)
+void shuffle(std:vector<Data>, int n)
 {
-	dotprod = 0;	
-	for (int i =0; i<x.size(), i++){
-		dotprod+= (x[i] * w[i]);		
-	}
+    // shuffle the first "n" elements of the vector
+    return;
 }
 
-std::vector<float> p_train(std::vector<Data> data, int epochs, bool shuffled=0) 
+float dotprod(float x, std::vector<float> w)
 {
-    std::vector<float> w;//initializes to all 0's
+	float retval = 0;	
+	for (int i =0; i<w.size(), i++) {
+		retval += (x * w[i]);
+	}
+	return retval;
+}
 
-    std::vector<float> u;
-    u.resize(data.size());
-    for (int i=0; i<data.size(); i++) {
-        for (int ep=0; ep<epochs; ep++) {
-            u[i] = data[i].x * w[i];
-            if (data[i].y * u[i] <= 0) {
-                w = w + data[i].y * data[i].x;
+std::vector<float> p_train(std::vector<Data> data, int epochs, bool shuffled=0)
+{
+    std::vector<float> w;  //initializes to all 0's
+
+    bool updated = true;
+    while (updated) {
+        if (shuffled) 
+            shuffle(data, epochs);
+        updated = false;
+        for (int i=0; i<epochs; i++) {
+            if ((dotprod(data[i].x, w) * data[i].y) <= 0) {
+                for (int j=0; j<w.size(); j++) {
+                    w[j] = data[i].y * data[j].x + w[j];
+                }
+                updated = true;
             }
         }
     }
-
-
     return w;
 }
 
 /* Python version
  * def p_train(xs,  ys, shuffled=False, epoch=0):
  * 		w=list(repeat(0, len(xs[0]))
- * 		dotprod=lambda x,w: 
- * 			sum(imap(mul, x, w)
- * 			updated =True
+ * 		dotprod=lambda x,w:sum(imap(mul, x, w)
+ * 		updated =True
  *		data = zip(xs, ys)
  *		if epoch:
+			//pack input vectors with outputs
  *			data= data[:epoch]
  *			while updated:
  *				if shuffled:
  *					random.shuffle(data)
  *				updated = False
  *				for x,y in data:
+					//check if correctly classified
  *					if dotprod(x,w) * y <=0:
  *						w = [y* xi + wi fpr xi wi in izip(x,w)]
  *						updated=True
- *			return lambda x: -(w[0] + x * w[1]/w[2]
+			//return a function creates the learned decision boundary
+ *			return lambda x: -(w[0] + x * w[1])/w[2]
+			//y = mx + b
+			//y_guess_line = -(w[1]x + w[0]) / w[2] 
  */
 
 
